006a17b5460aebb247d241b10a58191557f9fc4c
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-09-10 21:36:21 +0100

    Time: 7 ms (43.8%), Space: 25.7 MB (46.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,24 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        copy = nums[:]
-        for x in range(n):
-            if x + k < n:
-                nums[x+k] = copy[x]
-            else:
-                rem = (x + k) % n
-                nums[rem] = copy[x]
-
         
+        # n = len(nums)
+        # copy = nums[:]
+        # for x in range(n):
+        #     if x + k < n:
+        #         nums[x+k] = copy[x]
+        #     else:
+        #         rem = (x + k) % n
+        #         nums[rem] = copy[x]
+
+        #reverse the array 3 times
+
+        def rev(l, r):
+            while l<r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        n = len(nums)
+        k %= n
+        rev(0,n-1)
+        rev(0,k-1)
+        rev(k, n-1)
