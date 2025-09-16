4a175c4dd5cefcd8336192d7714463fd083a540b
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-09-16 14:01:55 +0100

    Time: 0 ms (100%), Space: 18 MB (13.34%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,22 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        #eeasy peasy lemon squeezy
+        stack = []
+        res = ""
+
+        newPath = [p for p in path.split('/') if p]
+
+        for x in newPath:
+            if x == ".." and stack:
+                stack.pop()
+            elif x == ".":
+                continue
+            elif x != ".." and x != ".":
+                stack.append(x)
+        
+        for y in stack:
+            res+="/"+y
+        if not stack:
+            return "/"
+        
+        return res
