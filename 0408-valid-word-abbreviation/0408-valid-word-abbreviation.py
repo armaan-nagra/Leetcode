4a4d1d2001b706906e0dd0e20b428d2b35597751
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-10-24 15:26:18 +0100

    Time: 0 ms (100%), Space: 17.9 MB (16.95%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,37 +1,69 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        #manipulate string to get an arr of form ["i", 12, "iz", 4, "n"]
-        tokens = []
-        j = 0 
-        m = len(abbr)
-        while j < m:
-            if abbr[j].isalpha():
-                start = j 
-                while j < m and abbr[j].isalpha():
-                    j += 1
-                tokens.append(abbr[start:j])
-            else:
-                start = j
-                while j < m and abbr[j].isdigit():
-                    j += 1
-                num_str = abbr[start:j]
-                if num_str[0] == "0":
-                    return False
-                tokens.append(int(num_str))
+        i, j = 0, 0
 
-        i, n = 0, len(word)
-        for tok in tokens:
-            if isinstance(tok, int):
-                i += tok
-                if i > n:
-                    return False 
+        while i < len(word) and j < len(abbr):
+            if word[i] == abbr[j]:
+                i+=1
+                j+=1
+            elif abbr[j].isalpha() or abbr[j] == "0":
+                return False
             else:
-                k = len(tok)
-                if i + k > n or word[i:i+k] != tok:
-                    return False 
-                i += k 
+                sublen = 0
+                while j < len(abbr) and not abbr[j].isalpha():
+                    sublen = sublen * 10 + int(abbr[j])
+                    j += 1
+                i += sublen
+        return i == len(word) and j == len(abbr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        return i == n
+        # tokens = []
+        # j = 0 
+        # m = len(abbr)
+        # while j < m:
+        #     if abbr[j].isalpha():
+        #         start = j 
+        #         while j < m and abbr[j].isalpha():
+        #             j += 1
+        #         tokens.append(abbr[start:j])
+        #     else:
+        #         start = j
+        #         while j < m and abbr[j].isdigit():
+        #             j += 1
+        #         num_str = abbr[start:j]
+        #         if num_str[0] == "0":
+        #             return False
+        #         tokens.append(int(num_str))
+
+        # i, n = 0, len(word)
+        # for tok in tokens:
+        #     if isinstance(tok, int):
+        #         i += tok
+        #         if i > n:
+        #             return False 
+        #     else:
+        #         k = len(tok)
+        #         if i + k > n or word[i:i+k] != tok:
+        #             return False 
+        #         i += k 
+        
+        # return i == n
 
 
 
