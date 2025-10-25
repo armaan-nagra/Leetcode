773c2f07da64e3e4501b25c35dac0082a4720f93
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-10-25 22:06:14 +0100

    Time: 4 ms (85.65%), Space: 18.3 MB (14.7%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,21 +1,50 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        one_set = set()
+        bed = [0] + flowerbed + [0]
+        length = len(bed)
 
-        for i, x in enumerate(flowerbed):
-            if x == 1:
-                one_set.add(i)
-        
+        for i in range(1, length - 1):
+            if bed[i] == 0 and bed[i-1] == 0 and bed[i+1] == 0:
+                bed[i] = 1
+                n-=1
 
-        counter = 0
-        length = len(flowerbed)
-        adds = 0
-        while counter < length:
-            if counter not in one_set and counter - 1 not in one_set and counter + 1 not in one_set:
-                one_set.add(counter)
-                adds += 1
-            counter += 1
+                if n == 0:
+                    return True
+            
+        return n <= 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # one_set = set()
+
+        # for i, x in enumerate(flowerbed):
+        #     if x == 1:
+        #         one_set.add(i)
         
-        return adds >= n
+        # counter = 0
+        # length = len(flowerbed)
+        # adds = 0
+        # while counter < length:
+        #     if counter not in one_set and counter - 1 not in one_set and counter + 1 not in one_set:
+        #         one_set.add(counter)
+        #         adds += 1
+        #     counter += 1
+        
+        # return adds >= n
 
 
