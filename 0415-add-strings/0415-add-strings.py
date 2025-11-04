41530ea6a6a70adc0c15f9dce905eaed1ddde0f3
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-11-04 14:22:07 +0000

    Time: 0 ms (100%), Space: 18.1 MB (64.65%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,25 +1,35 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        counter = 1
+        res = []
         n = len(num1)
         m = len(num2)
+
         carry = 0
-        res = ""
-        while counter <= n or counter <= m:
-            m_total = 0
-            n_total = 0
-            if counter <= n:
-                n_total += ord(num1[n - counter]) - 48
-            if counter <= m:
-                m_total += ord(num2[m - counter]) - 48
-            total = carry + m_total + n_total
-            counter += 1
+
+        m_counter = 0
+        n_counter = 0
+
+        while m_counter < m or n_counter < n:
+            val_1 = 0
+            val_2 = 0
+            if n_counter < n:
+                val_1 = ord(num1[n-n_counter-1]) - ord('0')
+            if m_counter < m:
+                val_2 = ord(num2[m-m_counter-1]) - ord('0')
+            total = val_1 + val_2 + carry
+            
             carry = total // 10
-            value = total % 10
-            res = res + str(value)
-        if carry != 0:
-            res = res + (str(carry))
+            slide_in = total % 10
+            res.append(chr(ord('0') + slide_in))
+            m_counter += 1
+            n_counter += 1
         
-        return res[::-1]
+        if carry:
+            res.append(chr(ord('0') + carry))
+        
+        res.reverse()
+        return "".join(res)
+                
+        
 
         
