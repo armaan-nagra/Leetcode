dff52767d3e574b36682544eaea7f4d38afa986f
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-11-04 10:42:02 +0000

    Time: 163 ms (66.25%), Space: 12.4 MB (66.06%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,18 +1,31 @@
 class Solution(object):
     def countSubstrings(self, s):
-        n = len(s)
         res = 0
-        for i in range(n):
-            l, r = i,i
-            while l>=0 and r<n and s[l] == s[r]:
-                res += 1
-                l-=1
-                r+=1
-            
-            l, r = i, i + 1
 
-            while l>=0 and r<n and s[l] == s[r]:
+        for x in range(len(s)):
+            res += 1
+            i = x
+            j, k = i - 1, i + 1
+            while j >= 0 and k < len(s):
+                if s[j] == s[k]:
+                    res += 1
+                    j -= 1
+                    k += 1
+                else:
+                    break
+        
+        for x in range(len(s)-1):
+            a = x
+            b = x + 1
+            if s[a] == s[b]:
                 res += 1
-                l-=1
-                r+=1
+                c, d = a - 1, b + 1
+                while c >= 0 and d < len(s):
+                    if s[c] == s[d]:
+                        res += 1
+                        c -= 1
+                        d += 1
+                    else:
+                        break
+
         return res
