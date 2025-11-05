c5eeaabba7d823d3d49347ad196f848d702d6f29
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-11-05 19:03:54 +0000

    Time: 0 ms (100%), Space: 17.8 MB (49.24%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,29 +1,40 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        res = []
         n = len(s)
         i = 0
+
         while i < n and s[i] == " ":
             i += 1
         
         sign = 1
-        if i < n and (s[i] == "+" or s[i] == "-"):
-            sign = -1 if s[i] == "-" else 1
+        if i < n and (s[i] == "-" or s[i] == "+"):
+            if s[i] == "-":
+                sign = -1
+            else:
+                sign = 1
             i += 1
         
         num = 0
         while i < n and s[i].isdigit():
-            d = ord(s[i]) - ord('0')
-            num = num * 10 + d
+            #miss out any leading zeros
+            if num == 0 and s[i] == "0":
+                i += 1
+                continue
+            value = ord(s[i]) - ord('0')
+            num = num * 10 + value
             i += 1
+
+        if num * sign > 2**31 - 1:
+            return 2**31-1
+        if num * sign < -2**31:
+            return -2**31
+
+        return sign * num
         
-        num *= sign
-        INT_MAX, INT_MIN = 2**31-1, -2**31
-        if num > INT_MAX:
-            return INT_MAX
-        if num < INT_MIN:
-            return INT_MIN
-        return num
+
+
+
+
 
             
                 
