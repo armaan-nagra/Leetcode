8b7fa8f9b3fea63123a54a7ab5c5450219c10bd0
Armaan Nagra <99617719+armaan-nagra@users.noreply.github.com>
2025-11-08 14:51:38 +0000

    Time: 51 ms (30.44%), Space: 21.8 MB (76.73%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,51 +1,23 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        stack = [] #store items of type (char, counter)
-
-        for char in s:
-            if stack and stack[-1][0] == char:
-                stack[-1][1] += 1
-                if stack[-1][1] == k:
+        stack = [] #store (char, freq)
+        for ch in s:
+            if stack:
+                if stack[-1][0] == ch:
+                    freq = stack[-1][1]
                     stack.pop()
+                    stack.append((ch, freq + 1))
+                    if stack[-1][1] == k:     
+                        stack.pop()
+                else:
+                    stack.append((ch, 1))
             else:
-                stack.append([char, 1])
+                stack.append((ch, 1))
+        res = ""
+
+        for ch, freq in stack:
+            res += ch * int(freq)
         
-        result = ""
-        for char, counter in stack:
-            result += char * counter
-        
-        return result
-        
-            
+        return res
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # stack = []
-
-        # for x in s:
-        #     #keep checking last k elements in stack?
-        #     stack.append(x)
-        #     if len(stack) >= k:
-        #         value = stack[-1]
-        #         remove = True
-        #         for y in range(1,k+1):
-        #             if stack[-y] != value:
-        #                 remove = False
-        #         if remove:
-        #             stack = stack[0:-k]
-        # return "".join(stack)
-                    
